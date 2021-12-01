85d1370115abeb2bd815b4bdf8c9f0bbce978ea5
Kevin Roche <kevinroche222@gmail.com>
2021-12-01 16:59:04 -0500

    initial commit of dataProcessing script

---
+++ scripts/01 - dataProcessing.py
@@ -0,0 +1,36 @@
+"""
+This script pulls the metadata I need to analyze my iTunes library and writes it to a .csv in the data folder.
+"""
+
+## Load packages
+from appscript import *
+import pandas as pd
+
+## Define iTunes application
+itunes = app("iTunes")
+
+## Initialize empty list
+iTunesInfo = []
+
+## Pull required data
+for track in itunes.file_tracks():
+    iTunesInfo.append(
+        {
+            "song": track.name(),
+            "artist": track.artist(),
+            "albumArtist": track.album_artist(),
+            "playCount": track.played_count(),
+            "dateAdded": track.date_added(),
+            "genre": track.genre(),
+            "duration": track.duration(),
+        }
+    )
+
+## Convert to pandas dataframe
+iTunesInfo = pd.DataFrame(iTunesInfo)
+
+## Write to folder
+iTunesInfo.to_csv(
+    "/Users/kevinroche22/PythonData/iTunesWrapped/data/iTunesInfo.csv",
+    index=False,
+)
